833137f07820d7bdc42ac6a36acadfd2c0e828a0
AdKiNaTOr <arslannormuminovxbox@gmail.com>
2026-02-05 23:05:08 +0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,15 @@ window.onload = function () {
   let h1text = document.getElementById('h1text')
   let targetText = "Ты уверен малой назад пути нет🙃"
   let onClickedYes = false
+  let sentButton = document.getElementById('sent')
+  let adkinator = document.getElementById('adkinat')
+  let gender = document.getElementsByName('gender')
+  let date = document.getElementById('date')
+  let name = document.getElementById('name')
+  let lastName = document.getElementById('lastname')
+  let andText = document.getElementById('and')
+  let sthrafGameText = document.getElementById('schtrafgame')
+  let buttonToSthrafGame = document.getElementById('button')
   targetArray =[...targetText]
   if (goto||text) {
     goto.addEventListener("click", function() {
@@ -27,10 +36,35 @@ window.onload = function () {
 
       } else {
 
-        window.location.href = 'https://google.com'
+        window.location.href = 'yourInformation.html'
 
       }
 
+
     });
   }
+  if (sentButton && adkinator && gender && date && name && lastName) {
+   sentButton.addEventListener('click', function() {
+     if (date.value != "" && name.value != "null" && gender.value != "null" && lastName != "null") {
+
+     adkinator.hidden = false;
+     adkinator.classList.add("adkinator");
+     setTimeout(function () {
+       window.location = "adkinatoorrrr.html"
+     }, 6000);
+   }
+   });
+ }
+
+ if (andText && sthrafGameText && buttonToSthrafGame) {
+   setTimeout(function () {
+     andText.classList.add("makeVisible")
+     setTimeout(function() {
+       sthrafGameText.classList.add("makeVisible")
+       setTimeout(function(){
+         buttonToSthrafGame.classList.add("makeVisible")
+       }, 3000)
+     },3000)
+   }, 3000);
+ }
 }
